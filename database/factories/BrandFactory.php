ba6d9cfeb9edb4444d162a287ada0a3c9e557649
Rainbowsixteen <emil.wikhede@gmail.com>
2022-04-16 17:45:58 +0200

    Adding a BrandFactory to be able to create dummy-data in the Brands-table so that the endpoints can work.

---
+++ database/factories/BrandFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Brand>
+ */
+class BrandFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'brand_name' => $this->faker->company(),
+            'created_at' => now(),
+            'updated_at' => now()
+        ];
+    }
+}
